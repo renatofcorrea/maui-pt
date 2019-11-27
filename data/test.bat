c6bf384ccea0fc8dffac9c177f8572b1702fba1e
renatofcorrea <renatofcorrea@users.noreply.github.com>
2019-11-27 12:53:16 -0300

    update bats path
    update bats with new path to files

--- data/test.bat
+++ data/test.bat
@@ -1,3 +1,3 @@
 CHCP 65001
-java -Dfile.encoding=UTF-8 -jar ..\target\maui-pt.jar test -l docs\corpusci\full_texts\test30 -m models\testmodel -v  vocabulary\TBCI-SKOS_pt.rdf.gz -f skos -i pt
+java -Dfile.encoding=UTF-8 -jar ..\target\maui-pt.jar test -l docs\corpusci\fulltexts\test30 -m models\testmodel -v  vocabulary\TBCI-SKOS_pt.rdf.gz -f skos -i pt
 pause
