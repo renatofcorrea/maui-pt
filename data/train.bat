97848f0184aa9a42acfe6756e933fc1adaa736f9
rahmonjorge <44070986+rahmonjorge@users.noreply.github.com>
2019-12-06 11:08:43 -0300

    Delete train.bat

--- data/train.bat
+++
@@ -1,3 +0,0 @@
-CHCP 65001
-java -Dfile.encoding=UTF-8 -jar ..\target\maui-pt.jar train -l docs\corpusci\fulltexts\train30 -m models\testmodel -v vocabulary\TBCI-SKOS_pt.rdf -f skos -o 2 -i pt
-pause
